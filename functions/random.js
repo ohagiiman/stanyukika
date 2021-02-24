f5b21bc83141d4dff4cdba55bb3675ad1111aefc
Alexis <alexis.vielma.us@gmail.com>
2021-02-24 12:54:56 -0600

    test

--- functions/random.js
+++ functions/random.js
@@ -3,11 +3,12 @@ const fetch = require('node-fetch');
 const ENDPOINT = 'https://stanyukika.com/_songs.json';
 
 exports.handler = async (event, context) => {
-	const request = await fetch(ENDPOINT, {
-		headers: { Accept: 'application/json' },
-	});
+	// const request = await fetch(ENDPOINT, {
+	// 	headers: { Accept: 'application/json' },
+	// });
 
-	const { songs } = await request.json();
+	// const { songs } = await request.json();
+	const songs = ['https://umai.pw'];
 
 	const url = songs[~(Math.random() * songs.length)];
 
