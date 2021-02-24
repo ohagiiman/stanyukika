b7d6256cca5a420dd4b02a0ab75f2045cf61a37b
Alexis <alexis.vielma.us@gmail.com>
2021-02-24 13:23:19 -0600

    wor

--- functions/random.js
+++ functions/random.js
@@ -1,6 +1,18 @@
 exports.handler = async (event, context) => {
-	const songs = ['https://umai.pw'];
-	const url = songs[~~(Math.random() * songs.length)];
+	const songs = {
+		i_feel_love: 'https://youtu.be/xe605QH9BNs',
+		soul_lady: 'https://youtu.be/lxPndeAzfwI',
+		neon: 'https://youtu.be/Mm2PW9CZHJc',
+		yesterday: 'https://youtu.be/_cOFCiiQIvI',
+		a_day_for_love: 'https://youtu.be/CheuV7x7l5M',
+		pit_a_pet: 'https://youtu.be/RIKTYUWQOcs',
+		cherries_jubiles: 'https://youtu.be/giOCueVDtfg',
+		shade: 'https://youtu.be/rK69AriTIZc',
+		cherries_jubiles_aucustic: 'https://youtu.be/ypDTAszCZAk',
+		orange_road: 'https://youtu.be/Uoutn3GVHqs',
+	};
+
+	const url = Object.values(songs)[~~(Math.random() * songs.length)];
 
 	return {
 		statusCode: 301,
