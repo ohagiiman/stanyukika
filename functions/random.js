812084de986e21ef0e76150f8180e6562246634f
Alexis <alexis.vielma.us@gmail.com>
2021-02-24 13:41:01 -0600

    pls work

--- functions/random.js
+++ functions/random.js
@@ -1,7 +1,7 @@
 exports.handler = async (event, context) => {
 	const songs = Object.values({
-		i_feel_love: 'https://youtu.be/xe605QH9BNs',
 		soul_lady: 'https://youtu.be/lxPndeAzfwI',
+		i_feel_love: 'https://youtu.be/xe605QH9BNs',
 		neon: 'https://youtu.be/Mm2PW9CZHJc',
 		yesterday: 'https://youtu.be/_cOFCiiQIvI',
 		a_day_for_love: 'https://youtu.be/CheuV7x7l5M',
